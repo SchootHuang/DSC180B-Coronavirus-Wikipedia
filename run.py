82c69bdffc5d98fe408c1a91123d90577c0c97a0
Calvin Tam <c6tam@ucsd.edu>
2020-04-13 00:11:59 -0700

    Update wikipedia run.py configs

--- run.py
+++ run.py
@@ -4,11 +4,13 @@ import sys
 import json
 
 sys.path.insert(0, 'src') # add library code to path
-from src.etl import get_data, extract_data, get_wiki_category_articles, remove_dir
+from src.etl import get_data, extract_data, get_wiki_category_articles,\
+        remove_dir
 from src.media_etl import get_media_data, process_media_data
-from src.m_stat import get_m_stat_data, grab_m_stat_over_time
 
-DATA_PARAMS = 'config/data-params.json'
+EDIT_HISTORY_DATA_PARAMS = 'config/edit-history-data-params.json'
+PAGEVIEWS_HISTORY_DATA_PARAMS = 'config/pageviews-data-params.json'
+COVID_ARTICLES_DATA_PARAMS = 'covid-article-data-params.json'
 MEDIA_DATA_PARAMS  = 'config/media-data-params.json'
 MEDIA_PROCESS_PARAMS  = 'config/media-process-params.json'
 
@@ -29,9 +31,19 @@ def main(targets):
         remove_dir('data/out_m_stat')
 
     # make the data target
-    if 'data' in targets:
-        cfg = load_params(DATA_PARAMS)
+    if 'edit-history-data' in targets:
+        cfg = load_params(EDIT_HISTORY_DATA_PARAMS)
         get_data(**cfg)
+
+    # make the data target
+    if 'pageviews-data' in targets:
+        cfg = load_params(PAGEVIEWS_DATA_PARAMS)
+        get_data(**cfg)
+
+    # make the data target
+    if 'covid-data' in targets:
+        cfg = load_params(COVID_ARTICLES_DATA_PARAMS)
+        get_wiki_category_articles(**cfg)
 
     # make the media data target
     if 'media-data' in targets:
