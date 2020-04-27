3b6838cda1654e954a08ed0da158420cc77c0f9e
c6tam <c6tam@c6tam-18969.pods.c6tam.svc.cluster.local>
2020-04-26 19:28:05 -0700

    Temporary Sankey Graph placeholder

---
+++ src/sankey.py
@@ -0,0 +1,62 @@
+def genSankey(df,cat_cols=[],value_cols='',title='Sankey Diagram'):
+    colorPalette = ['#142459','#176BA0','#19AADE','#1AC9E6','#1DE4BD',
+            '#6DF0D2', '#C7F9EE']
+    labelList = []
+    colorNumList = []
+    for catCol in cat_cols:
+        labelListTemp =  list(set(df[catCol].values))
+        colorNumList.append(len(labelListTemp))
+        labelList = labelList + labelListTemp
+
+    # remove duplicates from labelList
+    labelList = list(dict.fromkeys(labelList))
+
+    # define colors based on number of levels
+    colorList = []
+    for idx, colorNum in enumerate(colorNumList):
+        colorList = colorList + [colorPalette[idx]]*colorNum
+
+    # transform df into a source-target pair
+    for i in range(len(cat_cols)-1):
+        if i==0:
+            sourceTargetDf = df[[cat_cols[i],cat_cols[i+1],value_cols]]
+            sourceTargetDf.columns = ['source','target','count']
+        else:
+            tempDf = df[[cat_cols[i],cat_cols[i+1],value_cols]]
+            tempDf.columns = ['source','target','count']
+            sourceTargetDf = pd.concat([sourceTargetDf,tempDf])
+        sourceTargetDf = sourceTargetDf.groupby(['source','target']).agg({'count':'sum'}).reset_index()
+
+    # add index for source-target pair
+    sourceTargetDf['sourceID'] = sourceTargetDf['source'].apply(lambda x: labelList.index(x))
+    sourceTargetDf['targetID'] = sourceTargetDf['target'].apply(lambda x: labelList.index(x))
+
+    # creating the sankey diagram
+    data = dict(
+        type='sankey',
+        node = dict(
+          pad = 15,
+          thickness = 20,
+          line = dict(
+            color = "black",
+            width = 0.5
+          ),
+          label = labelList,
+          color = colorList
+        ),
+        link = dict(
+          source = sourceTargetDf['sourceID'],
+          target = sourceTargetDf['targetID'],
+          value = sourceTargetDf['count']
+        )
+      )
+
+    layout =  dict(
+        title = title,
+        font = dict(
+          size = 10
+        )
+    )
+
+    fig = dict(data=[data], layout=layout)
+    return fig
