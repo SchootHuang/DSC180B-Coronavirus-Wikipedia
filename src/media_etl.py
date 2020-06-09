e7bba246d2cff00e2546cd8a159cf704c495446b
SchootHuang <36254098+SchootHuang@users.noreply.github.com>
2020-06-08 20:39:31 -0700

    Update media_etl.py

--- src/media_etl.py
+++ src/media_etl.py
@@ -3,6 +3,7 @@ import zipfile
 import os
 
 def get_media_data(**cfg):
+    '''retrive media dataset'''
     for url in cfg['URLs']:
         print(url)
         infile = os.path.join(cfg['outpath'],url.split('/')[-1])
@@ -13,6 +14,8 @@ def get_media_data(**cfg):
         zip_ref.extractall(infile)
         
 def process_media_data(**cfg):
+    '''get COVID-19 related news out of news dataset'''
+
     result = []
     for chunk in pd.read_csv(cfg['infile'],usecols=range(2,12),
                          parse_dates=['date'], chunksize=chunksize):
